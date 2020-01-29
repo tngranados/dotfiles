dc38b95e3fb5e32ffd27bdac923c0905791878fe
tngranados <antoniogra7@gmail.com>
2020-01-29 16:50:28 +0100

    Update mac zsh keybindings

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -4,5 +4,15 @@ export LSCOLORS="EHfxcxdxBxegecabagacad"
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
-# Alt+left deletes word
-bindkey "^[" backward-kill-word
+# Text editing, this is meant to work with this iTerm2 key preset:
+# https://gist.github.com/tngranados/b1aef481755a6b893ac8fd3004bf701c
+bindkey '^[cmddelete' backward-kill-line
+bindkey '^[altdelete' backward-kill-word
+bindkey '^[altleft' backward-word
+bindkey '^[cmdleft' beginning-of-line
+bindkey '^[altright' forward-word
+bindkey '^[cmdright' end-of-line
+bindkey '^[cmddeleteforward' kill-line
+bindkey '^[altdeleteforward' kill-word
+bindkey '^[home' beginning-of-line
+bindkey '^[end' end-of-line
