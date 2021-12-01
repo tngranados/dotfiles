8db5558492c2ebc3940e837657d54169412f6dcd
Toni Granados <toni@tngranados.com>
2021-12-01 16:45:32 +0100

    Update finicky to use applescript based url opener

--- .finicky.js
+++ .finicky.js
@@ -1,5 +1,6 @@
 module.exports = {
-  defaultBrowser: "Safari",
+  // defaultBrowser: "Safari",
+  defaultBrowser: "com.apple.ScriptEditor.id.open-URL-focus",
   options: {
     hideIcon: true,
     checkForUpdate: false
@@ -9,19 +10,19 @@ module.exports = {
       match: /zoom\.us\/join/,
       browser: 'com.apple.ScriptEditor.id.open-URL'
     },
-    {
-      match: () => {
-        const startTime = 9 * 60;
-        const endTime = 18 * 60;
+    // {
+    //   match: () => {
+    //     const startTime = 9 * 60;
+    //     const endTime = 18 * 60;
 
-        const date = new Date();
-        const now = date.getHours() * 60 + date.getMinutes();
-        const isWeekend = date.getDay() == 6 || date.getDay() == 0;
+    //     const date = new Date();
+    //     const now = date.getHours() * 60 + date.getMinutes();
+    //     const isWeekend = date.getDay() == 6 || date.getDay() == 0;
 
-        return !isWeekend && startTime <= now && now < endTime;
-      },
-      browser: "Google Chrome"
-    }
+    //     return !isWeekend && startTime <= now && now < endTime;
+    //   },
+    //   browser: "Google Chrome"
+    // }
   ],
   rewrite: [
     {
