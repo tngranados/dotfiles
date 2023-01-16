22d15651c40bebc6f672800fe7a1b44597dba3f4
Toni Granados <toni@tngranados.com>
2023-01-16 09:20:59 +0100

    Improve and fix fzfgit commands

--- modules/fzfgit.zsh
+++ modules/fzfgit.zsh
@@ -7,8 +7,9 @@ fzfgit() {
     printf "${fn}: ${color}${desc}\033[0m\n"
   }
   echo_color "fbr" "checkout git branch (including remote branches)"
-  echo_color "fco" "checkout git branch/tag"
-  echo_color "fshow" "git commit browser"
+  echo_color "fco" "checkout git branch/tag, automatically checkouts if only one branch/tag"
+  echo_color "fco_preview" "checkout git branch/tag with preview"
+  echo_color "flog" "git commit browser"
   echo_color "fcoc" "checkout git commit"
   echo_color "fcs" "get git commit sha (ex: git rebase -i \`fcs\`)"
   echo_color "fstash" "list stashes - enter shows content, C-d diff against HEAD, C-b checks as branch"
@@ -20,7 +21,7 @@ fbr() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
   branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+           fzf-tmux --query "$1" -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
@@ -33,17 +34,42 @@ fco() {
     git branch --all | grep -v HEAD             |
     sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  
+  tagsAndBranches=$(echo "$tags"; echo "$branches")
+  # if there is only one branch, just check it out
+  echo $(echo "$tagsAndBranches" | wc -l)
+  if [ $(echo "$tagsAndBranches" | wc -l) -eq 1 ]; then
+    git checkout $(echo "$tagsAndBranches" | awk '{print $2}')
+    return
+  fi
+
   target=$(
-    (echo "$tags"; echo "$branches") |
-    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+    (echo "$tagsAndBranches") |
+    fzf-tmux -1 --query "$1" -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
   git checkout $(echo "$target" | awk '{print $2}')
 }
 
-# fshow - git commit browser
-fshow() {
+# fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
+fco_preview() {
+  local tags branches target
+  branches=$(
+    git --no-pager branch --all \
+      --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" \
+    | sed '/^$/d') || return
+  tags=$(
+    git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$branches"; echo "$tags") |
+    fzf --query "$1" --no-hscroll --no-multi -n 2 \
+        --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'") || return
+  git checkout $(awk '{print $2}' <<<"$target" )
+}
+
+# flog - git commit browser
+flog() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+  fzf --query "$1"  --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
@@ -55,7 +81,7 @@ FZF-EOF"
 fcoc() {
   local commits commit
   commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+  commit=$(echo "$commits" | fzf --query "$1" --tac +s +m -e) &&
   git checkout $(echo "$commit" | sed "s/ .*//")
 }
 
@@ -64,7 +90,7 @@ fcoc() {
 fcs() {
   local commits commit
   commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+  commit=$(echo "$commits" | fzf --query "$1" --tac +s +m -e --ansi --reverse) &&
   echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
@@ -75,12 +101,14 @@ fcs() {
 # ctrl-b checks the stash out as a branch, for easier merging
 fstash() {
   local out q k sha
+  q="$1"
   while out=$(
     git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
     fzf --ansi --no-sort --query="$q" --print-query \
         --expect=ctrl-d,ctrl-b);
   do
-    mapfile -t out <<< "$out"
+    # mapfile -t out <<< "$out" # Bash only
+    out=("${(f)$(<<< "$out")}") # Zsh alternative
     q="${out[0]}"
     k="${out[1]}"
     sha="${out[-1]}"
@@ -103,7 +131,7 @@ ftags() {
   [ -e tags ] &&
   line=$(
     awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-    cut -c1-80 | fzf --nth=1,2
+    cut -c1-80 | fzf --query "$1" --nth=1,2
   ) && $EDITOR $(cut -f3 <<< "$line") -c "set nocst" \
                                       -c "silent tag $(cut -f2 <<< "$line")"
 }
