86c2f0aec9826a5b4dde7be5730399ffbb7a3063
Toni Granados <toni@tngranados.com>
2024-03-29 21:07:33 +0100

    Fix missing bin/local path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,5 +1,6 @@
 # Add dotfiles bin to PATH
 export PATH="$PATH:$DOTFILES/bin"
+export PATH="$PATH:$DOTFILES/bin/local"
 
 # Add yarn to PATH
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
