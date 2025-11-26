0b72a84a036d45df3e2b30a9a515c723606889bf
Toni Granados <toni@tngranados.com>
2025-11-26 15:29:04 +0100

    Add Antigravity bin to PATH

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -8,3 +8,6 @@ export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # Add cargo to PATH
 export PATH="$HOME/.cargo/bin:$PATH"
+
+# Added by Antigravity
+export PATH="/Users/tngranados/.antigravity/antigravity/bin:$PATH"
