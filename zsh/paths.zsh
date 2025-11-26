2d4225f097e339ae6353f420d7d6e94d7eb0617b
Toni Granados <toni@tngranados.com>
2025-11-26 15:45:22 +0100

    Refactor PATH configuration to avoid duplicates

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,13 +1,14 @@
-# Add dotfiles bin to PATH
-export PATH="$PATH:$DOTFILES/bin"
-export PATH="$PATH:$DOTFILES/bin/local"
-export PATH="$PATH:$DOTFILES/bin/basic"
+# Ensure no duplicates in PATH
+typeset -U path
 
-# Add yarn to PATH
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
-# Add cargo to PATH
-export PATH="$HOME/.cargo/bin:$PATH"
-
-# Added by Antigravity
-export PATH="/Users/tngranados/.antigravity/antigravity/bin:$PATH"
+# Define all paths at once
+path=(
+  "$HOME/.cargo/bin"
+  "$HOME/.yarn/bin"
+  "$HOME/.config/yarn/global/node_modules/.bin"
+  "$HOME/.antigravity/antigravity/bin"
+  $path
+  "$DOTFILES/bin"
+  "$DOTFILES/bin/local"
+  "$DOTFILES/bin/basic"
+)
